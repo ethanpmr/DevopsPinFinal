6abef6e112cbaf2a157c0c4dd1468c0708d4a8ed
ethanmartinez <ethanpmr@gmail.com>
2024-11-14 19:28:51 -0500

    Feat AMI Ubuntu
    se cambia la AMI para correr una VM Ubuntu

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ##Create and bootstrap webserver #lanzar
 resource "aws_instance" "webserver" {
-  ami                         = "ami-00c39f71452c08778"
+  ami                         = "ami-0866a3c8686eaeeba"
   instance_type               = "t2.micro"
   #key_name                    = app-ssh-key 
   associate_public_ip_address = true
